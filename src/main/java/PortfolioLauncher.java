c6e3e3a6940cdf85d94c3652001e476ce510c352
Gonkad <86889488+Gonkad@users.noreply.github.com>
2024-07-07 18:42:41 +0100

    update font

--- src/main/java/PortfolioLauncher.java
+++ src/main/java/PortfolioLauncher.java
@@ -1,15 +1,20 @@
 import gearth.extensions.ExtensionForm;
 import gearth.extensions.ExtensionFormCreator;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class PortfolioLauncher extends ExtensionFormCreator {
 
     @Override
     public ExtensionForm createForm(Stage primaryStage) throws Exception {
+        // Load the custom font
+        Font.loadFont(getClass().getResourceAsStream("/fonts/VolterGoldfish.ttf"), 10);
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Portfolio.fxml"));
         Parent root = loader.load();
 
